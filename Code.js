b1230ee08f874ade37f7aeace7c0b3311acc0869
Rovers,Zjuul Z <z.rovers@student.fontys.nl>
2021-02-24 15:05:56 +0100

    Update Code.js

--- Code.js
+++ Code.js
@@ -1,7 +1,20 @@
 let Button1 = document.getElementById("B1").addEventListener("click", B1Clicked)
-let Button2 = document.getElementById("B2")
-let Button3 = document.getElementById("B3")
+let Button2 = document.getElementById("B2").addEventListener("click", B2Clicked)
+let Button3 = document.getElementById("B3").addEventListener("click", B3Clicked)
+
+let Input
 
 function B1Clicked(){
-    console.log("Test")
+    Input = 1
+    console.log(Input)
+}
+
+function B2Clicked(){
+    Input = 2
+    console.log(Input)
+}
+
+function B3Clicked(){
+    Input = 3
+    console.log(Input)
 }
