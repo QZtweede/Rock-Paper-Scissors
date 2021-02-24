ae60480cac5b0fa92dcb39502063c293aac2f076
Rovers,Zjuul Z <z.rovers@student.fontys.nl>
2021-02-24 15:18:28 +0100

    Update Code.js

--- Code.js
+++ Code.js
@@ -3,10 +3,12 @@ let Button2 = document.getElementById("B2").addEventListener("click", B2Clicked)
 let Button3 = document.getElementById("B3").addEventListener("click", B3Clicked)
 
 let Input
+let botOutput
+let Result
 
 function B1Clicked(){
     Input = 0
-    console.log(Input)
+    console.log()
     RandomInputEnemy()
 }
 
@@ -25,4 +27,20 @@ function B3Clicked(){
 function RandomInputEnemy(){
     botOutput = Math.floor(Math.random() * 3)
     console.log(botOutput)
+    Outcome()
+}
+
+function Outcome(){
+    if(Input === botOutput){
+        Result = "Tie";
+        console.log("Result = " + Result)
+    }
+   
+    if(Input + 1 === botOutput){
+        if(Input === 3){
+        Input = 0
+        }
+        Result = "Lost"
+        console.log("Result = " + Result)
+    }
 }
