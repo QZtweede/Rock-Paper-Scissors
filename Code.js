e3338ed3e4b4a3add512749e411459ab57988cfe
Rovers,Zjuul Z <z.rovers@student.fontys.nl>
2021-02-24 15:01:32 +0100

    Update Code.js

---
+++ Code.js
@@ -0,0 +1,7 @@
+let Button1 = document.getElementById("B1").addEventListener("click", B1Clicked())
+let Button2 = document.getElementById("B2")
+let Button3 = document.getElementById("B3")
+
+B1Clicked();{
+    console.log("Test")
+}
